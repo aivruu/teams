f709bbd45a3827717b1c2fa3d835b0f307c8df36
aivruu <benjaalejandro206@gmail.com>
2025-01-07 14:41:12 -0300

    feat(Permissions): create plugin-permissions enum

---
+++ plugin/src/main/java/io/github/aivruu/teams/permission/application/Permissions.java
@@ -0,0 +1,39 @@
+// This file is part of teams, licensed under the GNU License.
+//
+// Copyright (c) 2024 aivruu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+package io.github.aivruu.teams.permission.application;
+
+import org.jetbrains.annotations.NotNull;
+
+public enum Permissions {
+  CREATE("teams.command.create"),
+  DELETE("teams.command.delete"),
+  SELECT("teams.command.select"),
+  MODIFY("teams.command.modify"),
+  MENU("teams.command.menu"),
+  RELOAD("teams.command.reload"),
+  HELP("teams.command.help");
+
+  private final String node;
+
+  Permissions(final @NotNull String node) {
+    this.node = node;
+  }
+
+  public @NotNull String node() {
+    return this.node;
+  }
+}
