75851e783b347a0850959906f1127b0d6f9f0d8b
aivruu <benjaalejandro206@gmail.com>
2025-01-07 14:45:57 -0300

    feat(MenuConstants): provide constants-class for menu's id sharing for distinct purposes

---
+++ plugin/src/main/java/io/github/aivruu/teams/menu/infrastructure/shared/MenuConstants.java
@@ -0,0 +1,25 @@
+// This file is part of teams, licensed under the GNU License.
+//
+// Copyright (c) 2024 aivruu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+package io.github.aivruu.teams.menu.infrastructure.shared;
+
+public final class MenuConstants {
+  public static final String TAGS_MENU_ID = "tags-menu";
+
+  private MenuConstants() {
+    throw new UnsupportedOperationException("This class is for utility.");
+  }
+}
