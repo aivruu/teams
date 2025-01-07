bd0391cd34ec0a81358c5df883c28734504677f9
aivruu <benjaalejandro206@gmail.com>
2025-01-06 22:52:58 -0300

    feat(AggregateRoot): define aggregate-root model for domain-entities handling by implementations

---
+++ api/src/main/java/io/github/aivruu/teams/aggregate/domain/AggregateRoot.java
@@ -0,0 +1,48 @@
+// This file is part of teams, licensed under the GNU License.
+//
+// Copyright (c) 2024 aivruu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+package io.github.aivruu.teams.aggregate.domain;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * An aggregate-root model.
+ *
+ * @since 0.0.1
+ */
+public abstract class AggregateRoot {
+  private final String id;
+
+  /**
+   * Creates a new {@link AggregateRoot} with the given id.
+   *
+   * @param id this {@link AggregateRoot}'s id.
+   * @since 0.0.1
+   */
+  protected AggregateRoot(final @NotNull String id) {
+    this.id = id;
+  }
+
+  /**
+   * Returns this {@link AggregateRoot}'s id.
+   *
+   * @return The {@link AggregateRoot}'s id.
+   * @since 0.0.1
+   */
+  public final @NotNull String id() {
+    return this.id;
+  }
+}
