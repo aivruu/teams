44c72e2eb03ba44642634dd54f8a3b8d6a757ec5
aivruu <benjaalejandro206@gmail.com>
2025-02-08 23:15:49 -0300

    chore(MiniMessageHelper): add documentation for class-header and functions

--- api/src/main/java/io/github/aivruu/teams/minimessage/application/MiniMessageHelper.java
+++ api/src/main/java/io/github/aivruu/teams/minimessage/application/MiniMessageHelper.java
@@ -23,6 +23,11 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This class is used to proportionate to {@link Component} text-parsing with {@link MiniMessage}.
+ *
+ * @since 0.0.1
+ */
 public final class MiniMessageHelper {
   private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
 
@@ -30,10 +35,25 @@ public final class MiniMessageHelper {
     throw new UnsupportedOperationException("This class is for utility and cannot be instantiated.");
   }
 
+  /**
+   * Parses the given string to a {@link Component} and applies the given {@link TagResolver}s.
+   *
+   * @param message the text to parse.
+   * @param resolvers the text's placeholders.
+   * @return A {@link Component}.
+   * @since 0.0.1
+   */
   public static @NotNull Component text(final @NotNull String message, final @NotNull TagResolver... resolvers) {
     return message.isEmpty() ? Component.empty() : MINI_MESSAGE.deserialize(message, resolvers);
   }
 
+  /**
+   * Parses the given array's content to {@link Component}s.
+   *
+   * @param content the array to parse.
+   * @return A {@link Component}.
+   * @since 0.0.1
+   */
   public static @NotNull Component list(final @NotNull String[] content) {
     if (content.length == 0) {
       return Component.empty();
@@ -46,6 +66,14 @@ public final class MiniMessageHelper {
     return componentBuilder.build();
   }
 
+  /**
+   * Parses the given array's content to {@link Component}s and applies the given {@link TagResolver}s.
+   *
+   * @param content the array to parse.
+   * @param resolvers the text's placeholders.
+   * @return A {@link Component} array.
+   * @since 0.0.1
+   */
   public static @NotNull Component[] array(final @NotNull String[] content, final @NotNull TagResolver... resolvers) {
     final Component[] components = new Component[content.length];
     for (byte i = 0; i < components.length; i++) {
@@ -55,6 +83,13 @@ public final class MiniMessageHelper {
     return components;
   }
 
+  /**
+   * Parses the given {@link Component} to a string.
+   *
+   * @param component the component to parse.
+   * @return A string.
+   * @since 0.0.1
+   */
   public static @NotNull String string(final @NotNull Component component) {
     return MINI_MESSAGE.serialize(component);
   }
