9a39c021ce7d5d50af2bcd288f0ef6f81e29c6cb
aivruu <benjaalejandro206@gmail.com>
2025-02-08 21:50:44 -0300

    feat(TagModificationProcessor): create class to process the edit-mode's input and delegate it to the tag-modifier service

---
+++ api/src/main/java/io/github/aivruu/teams/tag/application/modification/TagModificationProcessor.java
@@ -0,0 +1,168 @@
+// This file is part of teams, licensed under the GNU License.
+//
+// Copyright (c) 2024 aivruu
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+package io.github.aivruu.teams.tag.application.modification;
+
+import io.github.aivruu.teams.minimessage.application.MiniMessageHelper;
+import io.github.aivruu.teams.plain.application.PlainComponentHelper;
+import io.github.aivruu.teams.result.domain.ValueObjectMutationResult;
+import io.github.aivruu.teams.tag.application.TagModifierService;
+import io.github.aivruu.teams.tag.domain.TagAggregateRoot;
+import io.github.aivruu.teams.tag.domain.TagPropertiesValueObject;
+import io.github.aivruu.teams.tag.domain.event.TagPropertyChangeEvent;
+import io.github.aivruu.teams.tag.domain.registry.TagAggregateRootRegistry;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * This class is used as base-class to proportionate basic and main logic for the modification-input
+ * processing based on the given context.
+ *
+ * @since 2.3.1
+ */
+public abstract class TagModificationProcessor {
+  private final JavaPlugin plugin; // Used for sync event-firing.
+  private final TagAggregateRootRegistry tagAggregateRootRegistry;
+  private final TagModifierService tagModifierService;
+
+  /**
+   * Creates a new {@link TagModificationProcessor} with the provided parameters.
+   *
+   * @param plugin a {@link JavaPlugin} instance.
+   * @param tagAggregateRootRegistry the {@link TagAggregateRootRegistry}.
+   * @param tagModifierService the {@link TagModifierService}.
+   * @since 2.3.1
+   */
+  protected TagModificationProcessor(
+    final @NotNull JavaPlugin plugin,
+    final @NotNull TagAggregateRootRegistry tagAggregateRootRegistry,
+    final @NotNull TagModifierService tagModifierService) {
+    this.plugin = plugin;
+    this.tagAggregateRootRegistry = tagAggregateRootRegistry;
+    this.tagModifierService = tagModifierService;
+  }
+
+  /**
+   * Processes the input (message) given based on the modification-context provided, and returns the context
+   * processed.
+   *
+   * @param player the player who is modifying the tag.
+   * @param modificationOnCurse the {@link ModificationOnCurseValueObject} for the tag's modification.
+   * @param message the edit-mode's input for modification.
+   * @return The {@link ModificationContext} processed, which can be.
+   * <ul>
+   * <li>{@link ModificationContext#FAILED} if the {@link TagPropertyChangeEvent} was cancelled.</li>
+   * <li>{@link ModificationContext#CANCELLED} if the player has cancelled the modification.</li>
+   * <li>See {@link #processContext(ModificationContext, TagAggregateRoot, String)} for more possible contexts.</li>
+   * </ul>
+   * @see #processContext(ModificationContext, TagAggregateRoot, String)
+   * @since 2.3.1
+   */
+  public @NotNull ModificationContext process(final @NotNull Player player, final @NotNull ModificationOnCurseValueObject modificationOnCurse, final @NotNull Component message) {
+    final TagPropertyChangeEvent tagPropertyChangeEvent = new TagPropertyChangeEvent(modificationOnCurse.tag(), modificationOnCurse.context());
+    // Avoid IllegalStateException due to asynchronous event-firing.
+    Bukkit.getScheduler().runTask(this.plugin, () -> Bukkit.getPluginManager().callEvent(tagPropertyChangeEvent));
+    if (tagPropertyChangeEvent.isCancelled()) {
+      return ModificationContext.FAILED;
+    }
+    final String input = PlainComponentHelper.plain(message);
+    if (input.equals("cancel")) {
+      return ModificationContext.CANCELLED;
+    }
+    return this.processContext(
+      modificationOnCurse.context(),
+      // Tag-existing validation is made previously during editor's command-execution, so the tag should exist.
+      this.tagAggregateRootRegistry.findInBoth(modificationOnCurse.tag()), input);
+  }
+
+  /**
+   * Process the input based on the given {@link ModificationContext} and provides a final {@link ModificationContext}
+   * for the tag-modification.
+   *
+   * @param context the {@link ModificationContext} to process the input.
+   * @param tagAggregateRoot the {@link TagAggregateRoot} to modify.
+   * @param input the input to process.
+   * @return A {@link ModificationContext} which can be.
+   * <ul>
+   * <li>{@link ModificationContext#CLEARED} (only for prefix/suffix) if property's content was cleared.</li>
+   * <li>{@link ModificationContext#CANCELLED} if the player has cancelled the modification.</li>
+   * <li>{@link ModificationContext#FAILED} if the property's content is the same as input.</li>
+   * <li>{@link ModificationContext#PREFIX} if the prefix was successfully updated.</li>
+   * <li>{@link ModificationContext#SUFFIX} if the suffix was successfully updated.</li>
+   * <li>{@link ModificationContext#COLOR} if the color was successfully updated.</li>
+   * </ul>
+   */
+  private @NotNull ModificationContext processContext(
+    final @NotNull ModificationContext context, final @NotNull TagAggregateRoot tagAggregateRoot, final @NotNull String input
+  ) {
+    return switch (context) {
+      case PREFIX -> {
+        if (input.equals("clear")) {
+          final TagPropertiesValueObject result = this.tagModifierService.updatePrefix(tagAggregateRoot, null).result();
+          // When second-parameters is null, the result will never be null.
+          tagAggregateRoot.tagComponentProperties(result);
+          yield ModificationContext.CLEARED;
+        }
+        final boolean status = this.validateResult(tagAggregateRoot, this.tagModifierService.updatePrefix(tagAggregateRoot, MiniMessageHelper.text(input + " ")));
+        yield status ? ModificationContext.PREFIX : ModificationContext.FAILED;
+      }
+      case SUFFIX -> {
+        if (input.equals("clear")) {
+          final TagPropertiesValueObject result = this.tagModifierService.updateSuffix(tagAggregateRoot, null).result();
+          // When second-parameters is null, the result will never be null.
+          tagAggregateRoot.tagComponentProperties(result);
+          yield ModificationContext.CLEARED;
+        }
+        final boolean status = this.validateResult(tagAggregateRoot, this.tagModifierService.updateSuffix(tagAggregateRoot, MiniMessageHelper.text(" " + input)));
+        yield status ? ModificationContext.SUFFIX : ModificationContext.FAILED;
+      }
+      case COLOR -> {
+        NamedTextColor color = NamedTextColor.NAMES.value(input);
+        if (color == null) {
+          color = NamedTextColor.WHITE;
+        }
+        final boolean status = this.validateResult(tagAggregateRoot, this.tagModifierService.updateColor(tagAggregateRoot, color));
+        yield status ? ModificationContext.COLOR : ModificationContext.FAILED;
+      }
+      // Below cases are not possible to be reached.
+      case FAILED -> ModificationContext.FAILED;
+      case CANCELLED -> ModificationContext.CANCELLED;
+      case CLEARED -> ModificationContext.CLEARED;
+      case NONE -> ModificationContext.NONE;
+    };
+  }
+
+  /**
+   * Validates the mutation-result provided by the internal {@link TagModifierService}'s methods.
+   *
+   * @param tagAggregateRoot the {@link TagAggregateRoot} to mutate after validation.
+   * @param mutationResult the {@link ValueObjectMutationResult}.
+   * @return Whether the mutation-result was {@link ValueObjectMutationResult#MUTATED_STATUS}.
+   * @see ValueObjectMutationResult#wasUnchanged()
+   * @since 2.3.1
+   */
+  private boolean validateResult(final @NotNull TagAggregateRoot tagAggregateRoot, final @NotNull ValueObjectMutationResult<TagPropertiesValueObject> mutationResult) {
+    if (mutationResult.wasUnchanged()) {
+      return false;
+    }
+    tagAggregateRoot.tagComponentProperties(/* Should never be null. */ mutationResult.result());
+    return true;
+  }
+}
