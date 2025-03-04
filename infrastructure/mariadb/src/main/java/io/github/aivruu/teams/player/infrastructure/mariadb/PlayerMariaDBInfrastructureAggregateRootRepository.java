4af86b65382356dacce6c4b5a8b086480b12f1d3
aivruu <benjaalejandro206@gmail.com>
2025-03-04 13:29:58 -0300

    refactor(mariadb-infrastructure): improve implementations by store the connection-pool, add more detailed logs and implement missing logic for existsAsync and deleteAsync methods

--- infrastructure/mariadb/src/main/java/io/github/aivruu/teams/player/infrastructure/mariadb/PlayerMariaDBInfrastructureAggregateRootRepository.java
+++ infrastructure/mariadb/src/main/java/io/github/aivruu/teams/player/infrastructure/mariadb/PlayerMariaDBInfrastructureAggregateRootRepository.java
@@ -16,10 +16,10 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 package io.github.aivruu.teams.player.infrastructure.mariadb;
 
-import com.zaxxer.hikari.HikariDataSource;
+import io.github.aivruu.teams.common.CommonMariaDBInfrastructureAggregateRootRepository;
+import io.github.aivruu.teams.logger.application.DebugLoggerHelper;
 import io.github.aivruu.teams.player.domain.PlayerAggregateRoot;
 import io.github.aivruu.teams.player.domain.PlayerModelEntity;
-import io.github.aivruu.teams.shared.infrastructure.CloseableInfrastructureAggregateRootRepository;
 import io.github.aivruu.teams.shared.infrastructure.StatementConstants;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -30,14 +30,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.concurrent.CompletableFuture;
 
-public final class PlayerMariaDBInfrastructureAggregateRootRepository implements CloseableInfrastructureAggregateRootRepository<PlayerAggregateRoot> {
-  private final HikariDataSource hikariDataSource;
+public final class PlayerMariaDBInfrastructureAggregateRootRepository extends CommonMariaDBInfrastructureAggregateRootRepository<PlayerAggregateRoot> {
+  private final Connection connectionPool;
   private final String tableName;
 
   public PlayerMariaDBInfrastructureAggregateRootRepository(
-    final @NotNull HikariDataSource hikariDataSource,
+    final @NotNull Connection connectionPool,
     final @NotNull String tableName) {
-    this.hikariDataSource = hikariDataSource;
+    this.connectionPool = connectionPool;
     this.tableName = tableName;
   }
 
@@ -45,14 +45,12 @@ public final class PlayerMariaDBInfrastructureAggregateRootRepository implements
   public boolean start() {
     return CompletableFuture
       .supplyAsync(() -> {
-        try (
-          final Connection connection = this.hikariDataSource.getConnection();
-          final PreparedStatement statement = connection.prepareStatement(
-            StatementConstants.CREATE_PLAYERS_DATA_TABLE_STATEMENT.formatted(this.tableName));
+        try (final PreparedStatement statement = this.connectionPool.prepareStatement(
+          StatementConstants.CREATE_PLAYERS_DATA_TABLE_STATEMENT.formatted(this.tableName))
         ) {
           return statement.execute();
         } catch (final SQLException exception) {
-          exception.printStackTrace();
+          DebugLoggerHelper.write("Unexpected exception when trying to create the database's player-data table.", exception);
           return false;
         }
       }, THREAD_POOL)
@@ -60,27 +58,20 @@ public final class PlayerMariaDBInfrastructureAggregateRootRepository implements
   }
 
   @Override
-  public void close() {
-    this.hikariDataSource.close();
-  }
-
-  @Override
   public @NotNull CompletableFuture<@Nullable PlayerAggregateRoot> findInPersistenceAsync(final @NotNull String id) {
     return CompletableFuture.supplyAsync(() -> {
-      try (
-        final Connection connection = this.hikariDataSource.getConnection();
-        final PreparedStatement statement = connection.prepareStatement(
-          StatementConstants.FIND_PLAYER_INFORMATION_STATEMENT.formatted(this.tableName))
+      try (final PreparedStatement statement = this.connectionPool.prepareStatement(
+        StatementConstants.FIND_PLAYER_INFORMATION_STATEMENT.formatted(this.tableName))
       ) {
+        statement.setString(1, id);
         final ResultSet resultSet = statement.executeQuery();
         if (!resultSet.next()) {
           return null;
         }
-        final String playerId = resultSet.getString("UUID");
-        final String selectedTag = resultSet.getString("tag");
-        return new PlayerAggregateRoot(playerId, new PlayerModelEntity(playerId, selectedTag.isEmpty() ? null : selectedTag));
+        final String selectedTag = resultSet.getString(1);
+        return new PlayerAggregateRoot(id, new PlayerModelEntity(id, selectedTag.isEmpty() ? null : selectedTag));
       } catch (final SQLException exception) {
-        exception.printStackTrace();
+        DebugLoggerHelper.write("Unexpected exception when trying to retrieve player's information from database.", exception);
         return null;
       }
     }, THREAD_POOL);
@@ -88,23 +79,31 @@ public final class PlayerMariaDBInfrastructureAggregateRootRepository implements
 
   @Override
   public @NotNull CompletableFuture<Boolean> existsAsync(final @NotNull String id) {
-    return null;
+    return CompletableFuture.supplyAsync(() -> {
+      try (final PreparedStatement statement = this.connectionPool.prepareStatement(
+        StatementConstants.FIND_PLAYER_INFORMATION_STATEMENT.formatted(this.tableName))
+      ) {
+        statement.setString(1, id);
+        return statement.executeQuery().next();
+      } catch (final SQLException exception) {
+        DebugLoggerHelper.write("Unexpected exception when trying to verify if player's data exists in database.", exception);
+        return false;
+      }
+    }, THREAD_POOL);
   }
 
   @Override
   public @NotNull CompletableFuture<Boolean> saveAsync(final @NotNull PlayerAggregateRoot aggregateRoot) {
     return CompletableFuture.supplyAsync(() -> {
-      try (
-        final Connection connection = this.hikariDataSource.getConnection();
-        final PreparedStatement statement = connection.prepareStatement(
-          StatementConstants.SAVE_PLAYER_INFORMATION_STATEMENT.formatted(this.tableName))
+      try (final PreparedStatement statement = this.connectionPool.prepareStatement(
+        StatementConstants.SAVE_PLAYER_INFORMATION_STATEMENT.formatted(this.tableName))
       ) {
         final String tag = aggregateRoot.playerModel().tag();
         statement.setString(1, aggregateRoot.id());
         statement.setString(2, (tag == null) ? "" : tag);
-        return statement.executeUpdate() >= 1;
+        return statement.executeUpdate() > 0;
       } catch (final SQLException exception) {
-        exception.printStackTrace();
+        DebugLoggerHelper.write("Unexpected exception when trying to save player's data to the database.", exception);
         return false;
       }
     }, THREAD_POOL);
@@ -112,6 +111,16 @@ public final class PlayerMariaDBInfrastructureAggregateRootRepository implements
 
   @Override
   public @NotNull CompletableFuture<Boolean> deleteAsync(final @NotNull String id) {
-    return null;
+    return CompletableFuture.supplyAsync(() -> {
+      try (final PreparedStatement statement = this.connectionPool.prepareStatement(
+        StatementConstants.DELETE_PLAYER_INFORMATION_STATEMENT.formatted(this.tableName))
+      ) {
+        statement.setString(1, id);
+        return statement.executeUpdate() > 0;
+      } catch (final SQLException exception) {
+        DebugLoggerHelper.write("Unexpected exception when trying to delete player's data from the database.", exception);
+        return false;
+      }
+    }, THREAD_POOL);
   }
 }
--- infrastructure/mariadb/src/main/java/io/github/aivruu/teams/tag/infrastructure/mariadb/TagMariaDBInfrastructureAggregateRootRepository.java
+++ infrastructure/mariadb/src/main/java/io/github/aivruu/teams/tag/infrastructure/mariadb/TagMariaDBInfrastructureAggregateRootRepository.java
@@ -16,8 +16,8 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 package io.github.aivruu.teams.tag.infrastructure.mariadb;
 
-import com.zaxxer.hikari.HikariDataSource;
-import io.github.aivruu.teams.shared.infrastructure.CloseableInfrastructureAggregateRootRepository;
+import io.github.aivruu.teams.common.CommonMariaDBInfrastructureAggregateRootRepository;
+import io.github.aivruu.teams.logger.application.DebugLoggerHelper;
 import io.github.aivruu.teams.shared.infrastructure.StatementConstants;
 import io.github.aivruu.teams.shared.infrastructure.util.JsonCodecHelper;
 import io.github.aivruu.teams.tag.domain.TagAggregateRoot;
@@ -32,14 +32,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.concurrent.CompletableFuture;
 
-public final class TagMariaDBInfrastructureAggregateRootRepository implements CloseableInfrastructureAggregateRootRepository<TagAggregateRoot> {
-  private final HikariDataSource hikariDataSource;
+public final class TagMariaDBInfrastructureAggregateRootRepository extends CommonMariaDBInfrastructureAggregateRootRepository<TagAggregateRoot> {
+  private final Connection connectionPool;
   private final String tableName;
 
   public TagMariaDBInfrastructureAggregateRootRepository(
-    final @NotNull HikariDataSource hikariDataSource,
+    final @NotNull Connection connectionPool,
     final @NotNull String tableName) {
-    this.hikariDataSource = hikariDataSource;
+    this.connectionPool = connectionPool;
     this.tableName = tableName;
   }
 
@@ -47,14 +47,12 @@ public final class TagMariaDBInfrastructureAggregateRootRepository implements Cl
   public boolean start() {
     return CompletableFuture
       .supplyAsync(() -> {
-        try (
-          final Connection connection = this.hikariDataSource.getConnection();
-          final PreparedStatement statement = connection.prepareStatement(
-            StatementConstants.CREATE_TAGS_DATA_TABLE_STATEMENT.formatted(this.tableName))
+        try (final PreparedStatement statement = this.connectionPool.prepareStatement(
+          StatementConstants.CREATE_TAGS_DATA_TABLE_STATEMENT.formatted(this.tableName))
         ) {
           return statement.execute();
         } catch (final SQLException exception) {
-          exception.printStackTrace();
+          DebugLoggerHelper.write("Unexpected exception when trying to create the database's tag-data table.", exception);
           return false;
         }
       }, THREAD_POOL)
@@ -62,30 +60,20 @@ public final class TagMariaDBInfrastructureAggregateRootRepository implements Cl
   }
 
   @Override
-  public void close() {
-    this.hikariDataSource.close();
-  }
-
-  @Override
   public @NotNull CompletableFuture<@Nullable TagAggregateRoot> findInPersistenceAsync(final @NotNull String id) {
     return CompletableFuture.supplyAsync(() -> {
-      try (
-        final Connection connection = this.hikariDataSource.getConnection();
-        final PreparedStatement statement = connection.prepareStatement(
-          StatementConstants.FIND_TAG_INFORMATION_STATEMENT.formatted(this.tableName))
+      try (final PreparedStatement statement = this.connectionPool.prepareStatement(
+        StatementConstants.FIND_TAG_INFORMATION_STATEMENT.formatted(this.tableName))
       ) {
+        statement.setString(1, id);
         final ResultSet resultSet = statement.executeQuery();
         if (!resultSet.next()) {
           return null;
         }
         final TagPropertiesValueObject properties = JsonCodecHelper.readProperties(resultSet.getString("properties"));
-        if (properties == null) {
-          return null;
-        }
-        final String tag = resultSet.getString("id");
-        return new TagAggregateRoot(tag, new TagModelEntity(tag, properties));
+        return (properties == null) ? null : new TagAggregateRoot(id, new TagModelEntity(id, properties));
       } catch (final SQLException exception) {
-        exception.printStackTrace();
+        DebugLoggerHelper.write("Unexpected exception when trying to retrieve tag's information from database.", exception);
         return null;
       }
     }, THREAD_POOL);
@@ -93,23 +81,30 @@ public final class TagMariaDBInfrastructureAggregateRootRepository implements Cl
 
   @Override
   public @NotNull CompletableFuture<Boolean> existsAsync(final @NotNull String id) {
-    return null;
+    return CompletableFuture.supplyAsync(() -> {
+      try (final PreparedStatement statement = this.connectionPool.prepareStatement(
+        StatementConstants.FIND_TAG_INFORMATION_STATEMENT.formatted(this.tableName))
+      ) {
+        statement.setString(1, id);
+        return statement.executeQuery().next();
+      } catch (final SQLException exception) {
+        DebugLoggerHelper.write("Unexpected exception when trying to verify if tag's data exists in database.", exception);
+        return false;
+      }
+    }, THREAD_POOL);
   }
 
   @Override
   public @NotNull CompletableFuture<Boolean> saveAsync(final @NotNull TagAggregateRoot aggregateRoot) {
     return CompletableFuture.supplyAsync(() -> {
-      try (
-        final Connection connection = this.hikariDataSource.getConnection();
-        final PreparedStatement statement = connection.prepareStatement(
-          StatementConstants.SAVE_TAG_INFORMATION_STATEMENT.formatted(this.tableName))
+      try (final PreparedStatement statement = this.connectionPool.prepareStatement(
+        StatementConstants.SAVE_TAG_INFORMATION_STATEMENT.formatted(this.tableName))
       ) {
-        final TagModelEntity tagModel = aggregateRoot.tagModel();
-        statement.setString(1, JsonCodecHelper.writeProperties(tagModel.tagComponentProperties()));
-        statement.setString(2, aggregateRoot.id());
-        return statement.executeUpdate() >= 1;
+        statement.setString(1, aggregateRoot.id());
+        statement.setString(2, JsonCodecHelper.writeProperties(aggregateRoot.tagModel().tagComponentProperties()));
+        return statement.executeUpdate() > 0;
       } catch (final SQLException exception) {
-        exception.printStackTrace();
+        DebugLoggerHelper.write("Unexpected exception when trying to save tag's data to the database.", exception);
         return false;
       }
     }, THREAD_POOL);
@@ -117,6 +112,16 @@ public final class TagMariaDBInfrastructureAggregateRootRepository implements Cl
 
   @Override
   public @NotNull CompletableFuture<Boolean> deleteAsync(final @NotNull String id) {
-    return null;
+    return CompletableFuture.supplyAsync(() -> {
+      try (final PreparedStatement statement = this.connectionPool.prepareStatement(
+        StatementConstants.DELETE_TAG_INFORMATION_STATEMENT.formatted(this.tableName))
+      ) {
+        statement.setString(1, id);
+        return statement.executeUpdate() > 0;
+      } catch (final SQLException exception) {
+        DebugLoggerHelper.write("Unexpected exception when trying to delete tag's data from the database.", exception);
+        return false;
+      }
+    }, THREAD_POOL);
   }
 }
