70dafb1c981fb4fad0f90298fa1902a540b4732f
aivruu <benjaalejandro206@gmail.com>
2025-05-08 19:05:51 -0400

    docs(api): add more documentation for APIs packages

---
+++ api/src/main/java/io/github/aivruu/teams/repository/domain/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides access to domain-repository interface-contract.
+ *
+ * @since 4.0.0
+ */
+package io.github.aivruu.teams.repository.domain;
---
+++ api/src/main/java/io/github/aivruu/teams/util/application/component/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides utility-class for adventure's {@link net.kyori.adventure.text.Component}s
+ * deserialization/serialization to plain-text or with MiniMessage.
+ *
+ * @since 1.0.0
+ */
+package io.github.aivruu.teams.util.application.component;
