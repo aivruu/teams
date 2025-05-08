4cff8567e304e11f6862a8147da4ca2e3634831d
aivruu <benjaalejandro206@gmail.com>
2025-05-08 19:10:39 -0400

    feat(player-cache-repository): implement "view" collection to provide information about cache-entries without allowing direct modifications to them

--- plugin/src/main/java/io/github/aivruu/teams/player/infrastructure/PlayerCacheAggregateRootRepository.java
+++ plugin/src/main/java/io/github/aivruu/teams/player/infrastructure/PlayerCacheAggregateRootRepository.java
@@ -24,37 +24,56 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.List;
 
 public final class PlayerCacheAggregateRootRepository implements PlayerAggregateRootRepository {
   private final Object2ObjectMap<String, PlayerAggregateRoot> cache = new Object2ObjectOpenHashMap<>();
+  private @Nullable Collection<PlayerAggregateRoot> valuesView;
 
   @Override
-  public @Nullable PlayerAggregateRoot findInCacheSync(final @NotNull String id) {
+  public @Nullable PlayerAggregateRoot findSync(final @NotNull String id) {
     return this.cache.get(id);
   }
 
   @Override
-  public boolean existsInCacheSync(final @NotNull String id) {
+  public boolean existsSync(final @NotNull String id) {
     return this.cache.containsKey(id);
   }
 
   @Override
-  public @NotNull Collection<PlayerAggregateRoot> findAllInCacheSync() {
-    return this.cache.values();
+  public @NotNull Collection<PlayerAggregateRoot> findAllSync() {
+    if (this.valuesView == null) {
+      this.valuesView = List.copyOf(this.cache.values());
+    }
+    return this.valuesView;
   }
 
   @Override
-  public void saveSync(final @NotNull PlayerAggregateRoot aggregateRoot) {
+  public void saveSync(final @NotNull String id, final @NotNull PlayerAggregateRoot aggregateRoot) {
     this.cache.put(aggregateRoot.id(), aggregateRoot);
+    if (this.valuesView == null) {
+      this.valuesView = List.copyOf(this.cache.values());
+    }
+    this.valuesView.add(aggregateRoot);
   }
 
   @Override
   public @Nullable PlayerAggregateRoot deleteSync(final @NotNull String id) {
-    return this.cache.remove(id);
+    final PlayerAggregateRoot playerAggregateRoot = this.cache.remove(id);
+    if (this.valuesView == null) {
+      this.valuesView = List.copyOf(this.cache.values());
+    }
+    if (playerAggregateRoot != null) {
+      this.valuesView.remove(playerAggregateRoot);
+    }
+    return playerAggregateRoot;
   }
 
   @Override
-  public void clearAllSync() {
+  public void clearSync() {
     this.cache.clear();
+    if (this.valuesView != null) {
+      this.valuesView.clear();
+    }
   }
 }
