b7bfe79894644c5e05be41aa9fb5dbbc511eb5e8
aivruu <benjaalejandro206@gmail.com>
2025-05-08 16:41:21 -0400

    refactor!: remove old-code in preparation for refactor for v4.0.0

--- api/src/main/java/io/github/aivruu/teams/logger/application/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides plugin debug-mode utility.
- *
- * @since 0.0.1
- */
-package io.github.aivruu.teams.logger.application;
--- api/src/main/java/io/github/aivruu/teams/minimessage/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides access to the application-layer MiniMessage format-parsing utilities.
- *
- * @since 2.3.1
- */
-package io.github.aivruu.teams.minimessage;
