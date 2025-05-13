2ba413ea27657c22cccae4904b6c8c543dff86be
aivruu <benjaalejandro206@gmail.com>
2025-05-12 21:11:54 -0400

    feat(MenuRepository): add DoNotCall annotation for methods

--- api/src/main/java/io/github/aivruu/teams/menu/application/repository/MenuRepository.java
+++ api/src/main/java/io/github/aivruu/teams/menu/application/repository/MenuRepository.java
@@ -16,6 +16,7 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 package io.github.aivruu.teams.menu.application.repository;
 
+import com.google.errorprone.annotations.DoNotCall;
 import io.github.aivruu.teams.menu.application.AbstractMenuModel;
 import io.github.aivruu.teams.repository.domain.DomainRepository;
 import org.jetbrains.annotations.NotNull;
@@ -32,6 +33,7 @@ public interface MenuRepository extends DomainRepository<AbstractMenuModel> {
    * @since 4.0.0
    */
   @Override
+  @DoNotCall
   default @NotNull Collection<AbstractMenuModel> findAllSync() {
     throw NOT_IMPLEMENTED_EXCEPTION;
   }
@@ -45,6 +47,7 @@ public interface MenuRepository extends DomainRepository<AbstractMenuModel> {
    * @since 4.0.0
    */
   @Override
+  @DoNotCall
   default <V> void updateSync(final @NotNull String id, @NotNull final V value) {
     throw NOT_IMPLEMENTED_EXCEPTION;
   }
