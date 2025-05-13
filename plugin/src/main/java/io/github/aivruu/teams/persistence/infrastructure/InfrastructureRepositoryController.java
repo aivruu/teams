84bc356c0333d6520dce46c59834aed676a38bea
aivruu <benjaalejandro206@gmail.com>
2025-05-12 21:01:34 -0400

    refactor(InfrastructureRepositoryController): make changes to structure to adapt it for configuration-manager use

--- plugin/src/main/java/io/github/aivruu/teams/persistence/infrastructure/InfrastructureRepositoryController.java
+++ plugin/src/main/java/io/github/aivruu/teams/persistence/infrastructure/InfrastructureRepositoryController.java
@@ -18,6 +18,7 @@ package io.github.aivruu.teams.persistence.infrastructure;
 
 import com.mongodb.client.MongoClient;
 import io.github.aivruu.teams.config.infrastructure.object.ConfigurationConfigurationModel;
+import io.github.aivruu.teams.config.infrastructure.ConfigurationManager;
 import io.github.aivruu.teams.util.application.Debugger;
 import io.github.aivruu.teams.persistence.infrastructure.utils.HikariInstanceProvider;
 import io.github.aivruu.teams.persistence.infrastructure.utils.MongoClientHelper;
@@ -41,76 +42,77 @@ import java.sql.Connection;
 
 public final class InfrastructureRepositoryController {
   private final Path dataFolder;
-  private final ConfigurationConfigurationModel configuration;
+  private final ConfigurationManager configurationManager;
   private InfrastructureAggregateRootRepository<PlayerAggregateRoot> playerInfrastructureAggregateRootRepository;
   private InfrastructureAggregateRootRepository<TagAggregateRoot> tagInfrastructureAggregateRootRepository;
 
-  public InfrastructureRepositoryController(final @NotNull Path dataFolder, final @NotNull ConfigurationConfigurationModel configuration) {
+  public InfrastructureRepositoryController(
+     final @NotNull Path dataFolder,
+     final ConfigurationManager configurationManager) {
     this.dataFolder = dataFolder;
-    this.configuration = configuration;
+    this.configurationManager = configurationManager;
   }
 
   public boolean selectAndInitialize() {
-    if (this.configuration.tagInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.JSON ||
-        this.configuration.playerInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.JSON) {
+    final ConfigurationConfigurationModel config = this.configurationManager.config();
+    if (config.tagInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.JSON ||
+        config.playerInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.JSON) {
       JsonCoder.buildWithAdapters(
         JsonTagAggregateRootCodec.INSTANCE, JsonTagPropertiesValueObjectCodec.INSTANCE, JsonPlayerAggregateRootCodec.INSTANCE);
     }
     // Database clients initialization if repositories require it.
-    if (this.configuration.playerInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.MONGODB ||
-        this.configuration.tagInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.MONGODB
+    if (config.playerInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.MONGODB ||
+        config.tagInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.MONGODB
     ) {
       Debugger.write("Initializing mongo-client instance with configuration's parameters.");
-      MongoClientHelper.buildClient(
-        this.configuration.host, this.configuration.username, this.configuration.database, this.configuration.password
-      );
+      MongoClientHelper.buildClient(config.host, config.username, config.database, config.password);
       // Check if parameters are valid and client was initialized correctly.
       if (MongoClientHelper.client() == null) {
         Debugger.write("Mongo-client couldn't be initialized correctly, stopping infrastructure repositories initialization.");
         return false;
       }
     }
-    if (this.configuration.playerInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.MARIADB ||
-        this.configuration.tagInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.MARIADB
+    if (config.playerInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.MARIADB ||
+        config.tagInfrastructureRepositoryType == InfrastructureAggregateRootRepository.Type.MARIADB
     ) {
       Debugger.write("Initializing mongo-client instance with configuration's parameters.");
-      HikariInstanceProvider.buildDataSource(
-        this.configuration.host, this.configuration.mariaDbPort, this.configuration.username,
-        this.configuration.database, this.configuration.password
-      );
+      HikariInstanceProvider.buildDataSource(config.host, config.mariaDbPort, config.username,
+         config.database, config.password);
       // Check if parameters are valid.
       if (HikariInstanceProvider.get() == null) {
         Debugger.write("HikariDataSource couldn't be initialized correctly, stopping infrastructure repositories initialization.");
         return false;
       }
     }
-    return this.determineAndInitializeInfrastructureTypes();
+    return this.determineAndInitializeInfrastructureTypes(config);
   }
 
-  private boolean determineAndInitializeInfrastructureTypes() {
+  private boolean determineAndInitializeInfrastructureTypes(
+     final @NotNull ConfigurationConfigurationModel config) {
     final Connection connection = HikariInstanceProvider.connection();
     final MongoClient client = MongoClientHelper.client();
-    this.playerInfrastructureAggregateRootRepository = switch (this.configuration.playerInfrastructureRepositoryType) {
+    this.playerInfrastructureAggregateRootRepository = switch (config.playerInfrastructureRepositoryType) {
       case JSON -> new PlayerJsonInfrastructureAggregateRootRepository(
-        this.dataFolder.resolve(this.configuration.playerCollectionAndDirectoryName));
+        this.dataFolder.resolve(config.playerCollectionAndDirectoryName));
       // Note: The client-instance could be null, but it won't throw a NullPointerException as this method is called
       // only when MongoDB infrastructure is required, at that point, the client, or it was initialized already, or
       // parameters were invalid and repository won't be initialized.
       case MONGODB -> new PlayerMongoInfrastructureAggregateRootRepository(
-        client, this.configuration.database, this.configuration.playerCollectionAndDirectoryName);
+        client, config.database, config.playerCollectionAndDirectoryName);
       case MARIADB -> new PlayerMariaDBInfrastructureAggregateRootRepository(
         // HikariDataSource instance shouldn't be null if repository-type is for MariaDB.
-        connection, this.configuration.playerCollectionAndDirectoryName);
+        connection, config.playerCollectionAndDirectoryName);
     };
-    this.tagInfrastructureAggregateRootRepository = switch (this.configuration.tagInfrastructureRepositoryType) {
+    this.tagInfrastructureAggregateRootRepository = switch (config.tagInfrastructureRepositoryType) {
       case JSON -> new TagJsonInfrastructureAggregateRootRepository(
-        this.dataFolder.resolve(this.configuration.tagCollectionAndDirectoryName));
+        this.dataFolder.resolve(config.tagCollectionAndDirectoryName));
       case MONGODB -> new TagMongoInfrastructureAggregateRootRepository(
-        client, this.configuration.database, this.configuration.tagCollectionAndDirectoryName);
+        client, config.database, config.tagCollectionAndDirectoryName);
       case MARIADB -> new TagMariaDBInfrastructureAggregateRootRepository(
-        connection, this.configuration.tagCollectionAndDirectoryName);
+        connection, config.tagCollectionAndDirectoryName);
     };
-    return this.playerInfrastructureAggregateRootRepository.start() && this.tagInfrastructureAggregateRootRepository.start();
+    return this.playerInfrastructureAggregateRootRepository.start()
+       && this.tagInfrastructureAggregateRootRepository.start();
   }
 
   public void close() {
