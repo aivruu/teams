16c33e16934f62e4f020b8aafd6abaa4d07b2a93
aivruu <benjaalejandro206@gmail.com>
2025-05-12 21:14:52 -0400

    docs(ProcessedContextResultValueObject): write documentation for full-class

--- api/src/main/java/io/github/aivruu/teams/tag/application/modification/context/ProcessedContextResultValueObject.java
+++ api/src/main/java/io/github/aivruu/teams/tag/application/modification/context/ProcessedContextResultValueObject.java
@@ -20,53 +20,134 @@ import io.github.aivruu.teams.tag.domain.TagAggregateRoot;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Represents the result that the plugin return when a modification has been passed through the
+ * {@link io.github.aivruu.teams.tag.application.modification.TagModificationProcessor}. This result
+ * is represented by a status-code, and the modified-tag's {@link TagAggregateRoot}.
+ *
+ * @param status the status-code for the modification.
+ * @param tagAggregateRoot the modified-tag's aggregate-root, or {@code null}.
+ * @since 4.0.0
+ */
 public record ProcessedContextResultValueObject(
    byte status,
    @Nullable TagAggregateRoot tagAggregateRoot) {
+  /** The tag specified does not exist. */
   public static final byte INVALID_TAG_AGGREGATE_ROOT = 0;
+  /** The user cancelled the modification. */
   public static final byte MODIFICATION_PROCESS_CANCELLED = 1;
+  /** The input-validating and checks were performed, delegated to implementation. */
   public static final byte PENDING_FOR_MODIFICATION_PROCESSING = 2;
+  /** The modification couldn't be applied. */
   public static final byte FAILED_MODIFICATION_PROCESS = 3;
+  /** The modification was done, and applied to the tag. */
   public static final byte MODIFIED_TAG_PROPERTIES = 4;
 
+  /**
+   * Creates a new {@link ProcessedContextResultValueObject} with the
+   * {@link #INVALID_TAG_AGGREGATE_ROOT} status-code, no aggregate-root provided.
+   *
+   * @return A new {@link ProcessedContextResultValueObject}.
+   * @since 4.0.0
+   */
   public static @NotNull ProcessedContextResultValueObject asInvalid() {
     return new ProcessedContextResultValueObject(INVALID_TAG_AGGREGATE_ROOT, null);
   }
 
+  /**
+   * Creates a new {@link ProcessedContextResultValueObject} with the
+   * {@link #MODIFICATION_PROCESS_CANCELLED} status-code, no aggregate-root provided.
+   *
+   * @return A new {@link ProcessedContextResultValueObject}.
+   * @since 4.0.0
+   */
   public static @NotNull ProcessedContextResultValueObject asCancelled() {
     return new ProcessedContextResultValueObject(MODIFICATION_PROCESS_CANCELLED, null);
   }
 
+  /**
+   * Creates a new {@link ProcessedContextResultValueObject} with the
+   * {@link #PENDING_FOR_MODIFICATION_PROCESSING} status-code.
+   *
+   * @param tagAggregateRoot the tag's aggregate-root.
+   * @return A new {@link ProcessedContextResultValueObject}.
+   * @since 4.0.0
+   */
   public static @NotNull ProcessedContextResultValueObject asPending(
      final @NotNull TagAggregateRoot tagAggregateRoot) {
     return new ProcessedContextResultValueObject(PENDING_FOR_MODIFICATION_PROCESSING, tagAggregateRoot);
   }
 
+  /**
+   * Creates a new {@link ProcessedContextResultValueObject} with the
+   * {@link #FAILED_MODIFICATION_PROCESS} status-code, no aggregate-root provided.
+   *
+   * @return A new {@link ProcessedContextResultValueObject}.
+   * @since 4.0.0
+   */
   public static @NotNull ProcessedContextResultValueObject asFailed() {
     return new ProcessedContextResultValueObject(FAILED_MODIFICATION_PROCESS, null);
   }
 
+  /**
+   * Creates a new {@link ProcessedContextResultValueObject} with the
+   * {@link #MODIFIED_TAG_PROPERTIES} status-code.
+   *
+   * @param tagAggregateRoot the tag's aggregate-root.
+   * @return A new {@link ProcessedContextResultValueObject}.
+   * @since 4.0.0
+   */
   public static @NotNull ProcessedContextResultValueObject asModified(
      final @NotNull TagAggregateRoot tagAggregateRoot) {
     return new ProcessedContextResultValueObject(MODIFIED_TAG_PROPERTIES, tagAggregateRoot);
   }
 
+  /**
+   * Returns whether this context-result's status-code is {@link #INVALID_TAG_AGGREGATE_ROOT}.
+   *
+   * @return true if that's the status-code, otherwise false.
+   * @since 4.0.0
+   */
   public boolean invalid() {
     return this.status == INVALID_TAG_AGGREGATE_ROOT;
   }
 
+  /**
+   * Returns whether this context-result's status-code is {@link #MODIFICATION_PROCESS_CANCELLED}.
+   *
+   * @return true if that's the status-code, otherwise false.
+   * @since 4.0.0
+   */
   public boolean cancelled() {
     return this.status == MODIFICATION_PROCESS_CANCELLED;
   }
 
+  /**
+   * Returns whether this context-result's status-code is {@link #PENDING_FOR_MODIFICATION_PROCESSING}.
+   *
+   * @return true if that's the status-code, otherwise false.
+   * @since 4.0.0
+   */
   public boolean pending() {
     return this.status == PENDING_FOR_MODIFICATION_PROCESSING;
   }
 
+  /**
+   * Returns whether this context-result's status-code is {@link #FAILED_MODIFICATION_PROCESS}.
+   *
+   * @return true if that's the status-code, otherwise false.
+   * @since 4.0.0
+   */
   public boolean failed() {
     return this.status == FAILED_MODIFICATION_PROCESS;
   }
 
+  /**
+   * Returns whether this context-result's status-code is {@link #MODIFIED_TAG_PROPERTIES}.
+   *
+   * @return true if that's the status-code, otherwise false.
+   * @since 4.0.0
+   */
   public boolean modified() {
     return this.status == MODIFIED_TAG_PROPERTIES;
   }
