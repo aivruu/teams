a9ac9630382d89b318778720aebaab00f69e687e
aivruu <benjaalejandro206@gmail.com>
2025-05-12 20:57:28 -0400

    style(ConfigurationContainer): apply code-style indentation to the code

--- plugin/src/main/java/io/github/aivruu/teams/config/infrastructure/ConfigurationContainer.java
+++ plugin/src/main/java/io/github/aivruu/teams/config/infrastructure/ConfigurationContainer.java
@@ -17,6 +17,7 @@
 package io.github.aivruu.teams.config.infrastructure;
 
 import io.github.aivruu.teams.TeamsPlugin;
+import io.github.aivruu.teams.config.application.ConfigurationInterface;
 import io.github.aivruu.teams.util.application.Debugger;
 import io.github.aivruu.teams.util.application.PluginExecutor;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
@@ -30,69 +31,74 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.CompletableFuture;
 
-public record ConfigurationContainer<C>(@NotNull C model, @NotNull HoconConfigurationLoader loader, @NotNull Class<C> modelClass) {
-  private static final ComponentLogger LOGGER = TeamsPlugin.getPlugin(TeamsPlugin.class).getComponentLogger();
+public record ConfigurationContainer<C>(
+   @NotNull C model,
+   @NotNull HoconConfigurationLoader loader,
+   @NotNull Class<C> modelClass) {
+  private static final ComponentLogger LOGGER =
+     TeamsPlugin.getPlugin(TeamsPlugin.class).getComponentLogger();
   private static final String CONFIG_HEADER = """
-    AldrTeams | A plugin that gives your players the ability to select customizable-tags and showcase them on
-    their name-tag and tab-list.
-
-    This plugin supports MiniMessage format for all features, menu-configuration, tags' prefixes/suffixes
-    and more, you can customize these tags as you want to implement custom gradient-prefixes or suffixes for
-    special-ranks.
-
-    All this process is handle through a customizable-menu, which provides a highly-customization both for
-    tags-selection as for another items, also, you can trigger actions when a player click on a specific item
-    on the menu (left and right-click actions), all this indications more detailed in the selector_menu file.
-
-    Almost all plugin's executable-actions requires two or more parameters, which are separated with the ';' char.
-    Single-parameter actions doesn't require that.
-    Actions:
-    - [SOUND] <sound-id>;<volume>;<pitch> - Plays a sound at the action's player-executor's location.
-    - [TITLE] <title>;<subtitle>;<fade-in>;<stay>;<fade-out> - Sends a title to the action's player-executor.
-    - [ACTION_BAR] <message> - Sends an action-bar message to the action's player-executor.
-    - [MESSAGE] <message> - Sends a message to the action's player-executor.
-    - [COMMAND] <PLAYER | CONSOLE>;<command> - Executes a command as the action's player-executor.
-    - [BROADCAST] <GLOBAL (all server) | LOCAL (world only)>;<message> - Broadcasts a message to all players.
-
-    The plugin provides its own placeholders (for PlaceholderAPI and MiniPlaceholders) to check player's selected-tag
-    properties, or check another tags globally:
-    - PlaceholderAPI:
-      %aldrteams_tag% - Returns the player's selected-tag's id.
-      %aldrteams_prefix% - Returns the prefix for the player's selected-tag.
-      %aldrteams_suffix% - Returns the suffix for the player's selected-tag.
-      %aldrteams_color% - Returns the color for the player's selected-tag.
-      %aldrteams_<tag-id>_prefix% - Returns the prefix for the specified tag-id.
-      %aldrteams_<tag-id>_suffix% - Returns the suffix for the specified tag-id.
-      %aldrteams_<tag-id>_color% - Returns the color for the specified tag-id.
-    - MiniPlaceholders:
-      <aldrteams_tag> - Returns the player's selected-tag's id.
-      <aldrteams_prefix> - Returns the prefix for the player's selected-tag.
-      <aldrteams_suffix> - Returns the suffix for the player's selected-tag.
-      <aldrteams_color> - Returns the color for the player's selected-tag.
-
-    List of available-colors for tags, these names can be used as input when modifying a tag's color in-game:
-    - black
-    - dark_blue
-    - dark_green
-    - dark_aqua
-    - dark_red
-    - dark_purple
-    - gold
-    - gray
-    - dark_gray
-    - blue
-    - green
-    - aqua
-    - red
-    - light_purple
-    - yellow
-    - white""";
+     AldrTeams | A plugin that gives your players the ability to select customizable-tags and showcase them on
+     their name-tag and tab-list.
+     
+     This plugin supports MiniMessage format for all features, menu-configuration, tags' prefixes/suffixes
+     and more, you can customize these tags as you want to implement custom gradient-prefixes or suffixes for
+     special-ranks.
+     
+     All this process is handle through a customizable-menu, which provides a highly-customization both for
+     tags-selection as for another items, also, you can trigger actions when a player click on a specific item
+     on the menu (left and right-click actions), all this indications more detailed in the selector_menu file.
+     
+     Almost all plugin's executable-actions requires two or more parameters, which are separated with the ';' char.
+     Single-parameter actions doesn't require that.
+     Actions:
+     - [SOUND] <sound-id>;<volume>;<pitch> - Plays a sound at the action's player-executor's location.
+     - [TITLE] <title>;<subtitle>;<fade-in>;<stay>;<fade-out> - Sends a title to the action's player-executor.
+     - [ACTION_BAR] <message> - Sends an action-bar message to the action's player-executor.
+     - [MESSAGE] <message> - Sends a message to the action's player-executor.
+     - [COMMAND] <PLAYER | CONSOLE>;<command> - Executes a command as the action's player-executor.
+     - [BROADCAST] <GLOBAL (all server) | LOCAL (world only)>;<message> - Broadcasts a message to all players.
+     
+     The plugin provides its own placeholders (for PlaceholderAPI and MiniPlaceholders) to check player's selected-tag
+     properties, or check another tags globally:
+     - PlaceholderAPI:
+       %aldrteams_tag% - Returns the player's selected-tag's id.
+       %aldrteams_prefix% - Returns the prefix for the player's selected-tag.
+       %aldrteams_suffix% - Returns the suffix for the player's selected-tag.
+       %aldrteams_color% - Returns the color for the player's selected-tag.
+       %aldrteams_<tag-id>_prefix% - Returns the prefix for the specified tag-id.
+       %aldrteams_<tag-id>_suffix% - Returns the suffix for the specified tag-id.
+       %aldrteams_<tag-id>_color% - Returns the color for the specified tag-id.
+     - MiniPlaceholders:
+       <aldrteams_tag> - Returns the player's selected-tag's id.
+       <aldrteams_prefix> - Returns the prefix for the player's selected-tag.
+       <aldrteams_suffix> - Returns the suffix for the player's selected-tag.
+       <aldrteams_color> - Returns the color for the player's selected-tag.
+     
+     List of available-colors for tags, these names can be used as input when modifying a tag's color in-game:
+     - black
+     - dark_blue
+     - dark_green
+     - dark_aqua
+     - dark_red
+     - dark_purple
+     - gold
+     - gray
+     - dark_gray
+     - blue
+     - green
+     - aqua
+     - red
+     - light_purple
+     - yellow
+     - white""";
 
   public @NotNull CompletableFuture<@Nullable ConfigurationContainer<C>> reload() {
     return CompletableFuture.supplyAsync(() -> {
       try {
         final C updatedModel = this.loader.load().get(this.modelClass);
-        return (updatedModel == null) ? null : new ConfigurationContainer<>(updatedModel, this.loader, this.modelClass);
+        return (updatedModel == null) ? null : new ConfigurationContainer<>(updatedModel,
+           this.loader, this.modelClass);
       } catch (final ConfigurateException exception) {
         Debugger.write("Unexpected exception during configuration reload: {}", exception);
         return null;
@@ -101,18 +107,18 @@ public record ConfigurationContainer<C>(@NotNull C model, @NotNull HoconConfigur
   }
 
   public static <C extends ConfigurationInterface> @Nullable ConfigurationContainer<C> of(
-    final @NotNull Path directory,
-    final @NotNull String fileName,
-    final @NotNull Class<C> modelClass
+     final @NotNull Path directory,
+     final @NotNull String fileName,
+     final @NotNull Class<C> modelClass
   ) {
     final Path path = directory.resolve(fileName + ".conf");
     final HoconConfigurationLoader loader = HoconConfigurationLoader.builder()
-      .prettyPrinting(true)
-      .defaultOptions(opts -> opts
-        .header(CONFIG_HEADER)
-        .shouldCopyDefaults(true))
-      .path(path)
-      .build();
+       .prettyPrinting(true)
+       .defaultOptions(opts -> opts
+          .header(CONFIG_HEADER)
+          .shouldCopyDefaults(true))
+       .path(path)
+       .build();
     try {
       final CommentedConfigurationNode commentedNode = loader.load();
       final C config = commentedNode.get(modelClass);
