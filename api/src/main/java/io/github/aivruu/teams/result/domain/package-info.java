9dbedd27415d52ab6a696af0d250dbd5c779f48a
aivruu <benjaalejandro206@gmail.com>
2025-05-12 21:09:14 -0400

    refactor!: remove old and no-longer used classes

--- api/src/main/java/io/github/aivruu/teams/result/domain/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides mutation-results provider for the ValueObjects.
- *
- * @since 0.0.1
- */
-package io.github.aivruu.teams.result.domain;
--- api/src/main/java/io/github/aivruu/teams/result/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides access to the domain-layer value-object mutation result-provider.
- *
- * @since 0.0.1
- */
-package io.github.aivruu.teams.result;
