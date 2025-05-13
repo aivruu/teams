c9d5a408c908929b58836c2af760f50b6aea7b99
aivruu <benjaalejandro206@gmail.com>
2025-05-12 21:02:49 -0400

    refactor(MenuCacheRepository): remove values-view field

--- plugin/src/main/java/io/github/aivruu/teams/menu/infrastructure/repository/MenuCacheRepository.java
+++ plugin/src/main/java/io/github/aivruu/teams/menu/infrastructure/repository/MenuCacheRepository.java
@@ -23,12 +23,8 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.List;
-
 public final class MenuCacheRepository implements MenuRepository {
   private final Object2ObjectMap<String, AbstractMenuModel> cache = new Object2ObjectOpenHashMap<>();
-  private Collection<AbstractMenuModel> valuesView;
 
   @Override
   public @Nullable AbstractMenuModel findSync(final @NotNull String id) {
@@ -41,37 +37,17 @@ public final class MenuCacheRepository implements MenuRepository {
   }
 
   @Override
-  public @NotNull Collection<AbstractMenuModel> findAllSync() {
-    if (this.valuesView == null) {
-      this.valuesView = List.copyOf(this.cache.values());
-    }
-    return this.valuesView;
-  }
-
-  @Override
   public void saveSync(final @NotNull String id, final @NotNull AbstractMenuModel object) {
     this.cache.put(object.id(), object);
-    if (this.valuesView == null) {
-      this.valuesView = List.copyOf(this.cache.values());
-    }
-    this.valuesView.add(object);
   }
 
   @Override
   public @Nullable AbstractMenuModel deleteSync(final @NotNull String id) {
-    final AbstractMenuModel menuModel = this.cache.remove(id);
-    if (this.valuesView == null) {
-      this.valuesView = List.copyOf(this.cache.values());
-    }
-    if (menuModel != null) {
-      this.valuesView.remove(menuModel);
-    }
-    return menuModel;
+    return this.cache.remove(id);
   }
 
   @Override
   public void clearSync() {
     this.cache.clear();
-    this.valuesView.clear();
   }
 }
