af63d167af59d311b00dac51918ecb748c1735cc
aivruu <benjaalejandro206@gmail.com>
2025-05-14 09:43:31 -0400

    chore(mariadb-infrastructure): rename mariadb-abstraction repository's package to 'mariadb'

--- infrastructure/mariadb/src/main/java/io/github/aivruu/teams/shared/infrastructure/mariadb/MariaDBInfrastructureAggregateRootRepository.java
+++ infrastructure/mariadb/src/main/java/io/github/aivruu/teams/shared/infrastructure/mariadb/MariaDBInfrastructureAggregateRootRepository.java
@@ -14,7 +14,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-package io.github.aivruu.teams.shared.infrastructure.common;
+package io.github.aivruu.teams.shared.infrastructure.mariadb;
 
 import com.zaxxer.hikari.HikariDataSource;
 import io.github.aivruu.teams.aggregate.domain.AggregateRoot;
