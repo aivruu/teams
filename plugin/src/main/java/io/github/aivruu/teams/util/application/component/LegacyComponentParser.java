243e45c6d5b6029e625a711a8d39ebdb9afee70b
aivruu <benjaalejandro206@gmail.com>
2025-05-19 21:39:49 -0400

    chore(LegacyComponentParser): move to 'util/application' package

--- plugin/src/main/java/io/github/aivruu/teams/util/application/component/LegacyComponentParser.java
+++ plugin/src/main/java/io/github/aivruu/teams/util/application/component/LegacyComponentParser.java
@@ -14,7 +14,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-package io.github.aivruu.teams.util.component;
+package io.github.aivruu.teams.util.application.component;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
