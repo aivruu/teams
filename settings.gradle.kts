bcfea7ea8f8b396f2bdafee7acfc514416b1dc92
aivruu <benjaalejandro206@gmail.com>
2026-04-17 02:00:10 -0400

    build(settings): rewrite settings-file for more-ordered projects inclusion, and define directory for plugin/common-logic management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,29 @@
 @file:Suppress("UnstableApiUsage")
 
+pluginManagement {
+  includeBuild("build-logic")
+}
+
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "teams"
 
-sequenceOf(
-  "api", "adapt", "plugin",
-  "infrastructure", "infrastructure-mariadb", "infrastructure-mongodb", "infrastructure-json"
-).forEach {
-  val kerbalProject = ":${rootProject.name}-$it"
+sequenceOf("api", "plugin", "infrastructure").forEach {
+  val kerbalProject = "${rootProject.name}-$it"
   include(kerbalProject)
-  if (it.startsWith("infrastructure-")) {
-    project(kerbalProject).projectDir = file("infrastructure/${it.substring(15)}")
-  } else {
-    project(kerbalProject).projectDir = file(it)
-  }
+  project(":$kerbalProject").projectDir = file(it)
+}
+
+sequenceOf("wrapper", "packet").forEach {
+  val kerbalProject = "${rootProject.name}-adapt-$it"
+  include(kerbalProject)
+  project(":$kerbalProject").projectDir = file("adapt/$it")
+}
+
+sequenceOf("json", "mongodb", "mariadb").forEach {
+  val kerbalProject = "${rootProject.name}-infrastructure-$it"
+  include(kerbalProject)
+  project(":$kerbalProject").projectDir = file("infrastructure/$it")
 }
