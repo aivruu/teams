9fcd82ac30afa9d29da7981280afde58957cf71d
aivruu <benjaalejandro206@gmail.com>
2026-04-19 19:56:25 -0400

    refactor(application/PlaceholderParser): refactor parsing-functions to accommodate with providers' API-changes

--- plugin/src/main/java/io/github/aivruu/teams/util/application/PlaceholderParser.java
+++ plugin/src/main/java/io/github/aivruu/teams/util/application/PlaceholderParser.java
@@ -1,6 +1,6 @@
 // This file is part of teams, licensed under the GNU License.
 //
-// Copyright (c) 2024-2025 aivruu
+// Copyright (c) 2024-2026 aivruu
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -18,13 +18,14 @@ package io.github.aivruu.teams.util.application;
 
 import io.github.aivruu.teams.util.application.component.MiniMessageParser;
 import io.github.miniplaceholders.api.MiniPlaceholders;
+import me.clip.placeholderapi.PAPIComponents;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -39,28 +40,28 @@ public final class PlaceholderParser {
     MODERN_PLACEHOLDERS_HOOKED = pluginManager.isPluginEnabled("MiniPlaceholders");
     // Initialize until this moment MiniPlaceholders its global-placeholders.
     if (MODERN_PLACEHOLDERS_HOOKED) {
-      MINIPLACEHOLDERS_GLOBAL_PLACEHOLDERS = MiniPlaceholders.getGlobalPlaceholders();
+      MINIPLACEHOLDERS_GLOBAL_PLACEHOLDERS = MiniPlaceholders.globalPlaceholders();
     } else {
       MINIPLACEHOLDERS_GLOBAL_PLACEHOLDERS = TagResolver.empty();
     }
   }
 
   public static @NotNull Component parseBoth(final @Nullable Player player, final @NotNull String text) {
-    // Firstly we parse it to [Component] for modern-placeholders processing, then we parse it to the
-    // legacy-format for PlaceholderAPI placeholders correct-parsing, and finally we convert it into
-    // a [Component].
-    final Component modern = MiniMessageParser.text(text, parseModern(player));
-    return LegacyComponentSerializer.legacyAmpersand().deserialize(parseLegacy(player, LegacyComponentSerializer.legacyAmpersand().serialize(modern)));
+    // Now that PlaceholderAPI supports components for placeholders we can avoid us the responsibility to handle manually legacy-text for
+    // those placeholders.
+    return PAPIComponents.setPlaceholders(player, MiniMessageParser.text(text, PlaceholderParser.parseModern()));
   }
 
   public static @NotNull Component[] parseBoth(final @Nullable Player player, final @NotNull String[] text) {
     final Component[] components = new Component[text.length];
     for (byte i = 0; i < text.length; i++) {
-      components[i] = parseBoth(player, text[i]);
+      components[i] = PlaceholderParser.parseBoth(player, text[i]);
     }
     return components;
   }
 
+  @Deprecated(since = "4.1.0")
+  @ApiStatus.ScheduledForRemoval(inVersion = "4.2.0")
   public static @NotNull TagResolver parseModern(final @Nullable Player player) {
     if (!MODERN_PLACEHOLDERS_HOOKED) {
       return TagResolver.empty();
@@ -68,11 +69,23 @@ public final class PlaceholderParser {
     final TagResolver.Builder builder = TagResolver.builder().resolver(MINIPLACEHOLDERS_GLOBAL_PLACEHOLDERS);
     // Should for audience specific-placeholders be included?
     if (player != null) {
-      builder.resolver(MiniPlaceholders.getAudiencePlaceholders(player));
+      // Deprecated due to MiniPlaceholders API changes, will be removed on the next major-release.
+      // -> MiniPlaceholders#audiencePlaceholders()
+      // builder.resolver(MiniPlaceholders.getAudiencePlaceholders(player));
     }
     return builder.build();
   }
 
+  public static @NotNull TagResolver parseModern() {
+    if (!MODERN_PLACEHOLDERS_HOOKED) return TagResolver.empty();
+
+    final TagResolver.Builder builder = TagResolver.builder().resolver(MINIPLACEHOLDERS_GLOBAL_PLACEHOLDERS);
+    builder.resolver(MiniPlaceholders.audiencePlaceholders());
+    return builder.build();
+  }
+
+  @Deprecated(since = "4.1.0")
+  @ApiStatus.ScheduledForRemoval(inVersion = "4.2.0")
   public static @NotNull String parseLegacy(final @Nullable Player player, final @NotNull String text) {
     return LEGACY_PLACEHOLDERS_HOOKED ? PlaceholderAPI.setPlaceholders(player, text) : text;
   }
